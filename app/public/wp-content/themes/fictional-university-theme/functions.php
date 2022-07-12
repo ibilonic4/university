ceafd15b92e5f74b6043e7c93bc561746460edca
ibilonic4 <ivorbilonic444@gmail.com>
2022-07-12 11:29:51 +0200

    ...

--- app/public/wp-content/themes/fictional-university-theme/functions.php
+++ app/public/wp-content/themes/fictional-university-theme/functions.php
@@ -23,4 +23,6 @@ add_theme_support('title-tag');
 add_action('after_setup_theme', 'university_features');
 
 
+
+
 ?>
--- app/public/wp-content/themes/fictional-university-theme/header.php
+++ app/public/wp-content/themes/fictional-university-theme/header.php
@@ -46,7 +46,9 @@
 
               ?>">About Us</a></li>
               <li><a href="#">Programs</a></li>
-              <li><a href="#">Events</a></li>
+              <li <?php
+              if (get_post_type()=='event'){echo 'class="current-menu-item"';}
+              ?> ><a href="<?php echo get_post_type_archive_link('event')?>">Events</a></li>
               <li><a href="#">Campuses</a></li>
               <li <?php
               
